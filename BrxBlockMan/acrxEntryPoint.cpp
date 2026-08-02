43a1dc0e987b67bf7ee22113a6ebd726bd96e9d9
Daniel <dwmcabinets@yahoo.com>
2026-08-02 16:10:22 +0800

    version v1.1.007

--- BrxBlockMan/acrxEntryPoint.cpp
+++ BrxBlockMan/acrxEntryPoint.cpp
@@ -86,7 +86,7 @@ public:
     {
         constexpr TCHAR MAJOR1 = '1';
         constexpr TCHAR MINOR1 = '1';
-        constexpr TCHAR REVISION1 = '0', REVISION2 = '0', REVISION3 = '6';
+        constexpr TCHAR REVISION1 = '0', REVISION2 = '0', REVISION3 = '7';
 
         constexpr unsigned int compileYear = (__DATE__[7] - '0') * 1000 + (__DATE__[8] - '0') * 100 + (__DATE__[9] - '0') * 10 + (__DATE__[10] - '0');
         constexpr unsigned int compileMonth = (__DATE__[0] == 'J') ? ((__DATE__[1] == 'a') ? 1 : ((__DATE__[2] == 'n') ? 6 : 7))    // Jan, Jun or Jul
@@ -113,15 +113,10 @@ public:
 
     static void ArxTemplate_blockman(void)
     {
-        if (!m_panel.m_hWnd)
+        if (m_panel.m_hWnd)
         {
-            m_panel.Create();
-            acutPrintf(L"\nPanel has been created");
-        }
-        else
-        {
-            m_panel.DestroyWindow();
-            acutPrintf(L"\nPanel has been Destroyed");
+            PRINTVER();
+            acutPrintf(L"\nPanel has been created: ");
         }
     }
 };
